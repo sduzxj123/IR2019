049183b6f7d3f826122f138d67831b20ffbc7213
xinjian zhao <2683258751@qq.com>
2019-11-22 17:45:39 +0800

    commit

---
+++ Data_mining_exp&report_four/Clustering.py
@@ -0,0 +1,235 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import cycle
+from time import time
+from sklearn import metrics
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.metrics import normalized_mutual_info_score
+from sklearn.mixture import GaussianMixture
+from sklearn.preprocessing import scale
+from sklearn.cluster import KMeans, AffinityPropagation, estimate_bandwidth, MeanShift, SpectralClustering, \
+    AgglomerativeClustering, DBSCAN
+
+#Loading  digits  data
+np.random.seed(42)
+digits = load_digits()
+data = scale(digits.data)
+n_samples, n_features = data.shape
+n_digits = len(np.unique(digits.target))
+labels = digits.target
+sample_size = 300
+print("n_digits: %d, \t n_samples %d, \t n_features %d"
+      % (n_digits, n_samples, n_features))
+print(82 * '_')
+print('init\t\ttime\tnmi\thomo\tcompl')
+
+#Evaluation
+def bench_k_means(estimator, name, data):
+    t0 = time()
+    estimator.fit(data)
+    print('%-9s\t%.2fs\t%.3f\t%.3f\t%.3f'
+          % (name, (time() - t0),
+             #metrics.normalized_mutual_info_score(labels, estimator.labels_),
+             metrics.v_measure_score(labels, estimator.labels_),
+             metrics.homogeneity_score(labels, estimator.labels_),
+             metrics.completeness_score(labels, estimator.labels_)))
+
+#kmeans
+bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),name="k-means++", data=data)
+bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=10),name="random", data=data)
+pca = PCA(n_components=n_digits).fit(data)
+bench_k_means(KMeans(init=pca.components_, n_clusters=n_digits, n_init=1),name="PCA-based",data=data)
+
+#AffinityPropagation
+bench_k_means(AffinityPropagation(),name="AffinityPropagation", data=data)
+
+# MeanShift
+bandwidth = estimate_bandwidth(data, quantile=0.1)
+bench_k_means(MeanShift(bandwidth=bandwidth, bin_seeding=True),name="MeanShift",data=data)
+
+# ward hierarchical clustering
+bench_k_means(AgglomerativeClustering(n_clusters=10, linkage='ward'),name="ward hierarchical clustering",data=data)
+
+# AgglomerativeClustering
+bench_k_means(AgglomerativeClustering(),name="AgglomerativeClustering",data=data)
+
+# DBSCN
+bench_k_means(DBSCAN(),name="DBSCAN()",data=data)
+
+print(82 * '_')
+reduced_data = PCA(n_components=2).fit_transform(data)
+
+# Visualize the results on PCA-reduced data  way1
+# example:meanshift
+
+bandwidth = estimate_bandwidth(reduced_data, quantile=0.07)
+meanshift = MeanShift(bandwidth=bandwidth, bin_seeding=True).fit(reduced_data)
+result = meanshift.labels_
+centroids = meanshift.cluster_centers_
+labels_unique = np.unique(result)
+n_clusters_ = len(labels_unique)
+plt.figure(1)
+plt.clf()
+colors = cycle('bgrcmybgrcmybgrcmybgrcmy')
+for k, col in zip(range(n_clusters_), colors):
+    my_members = result == k
+    cluster_center = centroids[k]
+    plt.plot(reduced_data[my_members, 0], reduced_data[my_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=5)
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+
+# Visualize the results on PCA-reduced data  way2
+# example:kmeans
+kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
+kmeans.fit(reduced_data)  # 对降维后的数据进行kmeans
+result = kmeans.labels_
+plt.figure(2)
+plt.clf()
+plt.scatter(reduced_data[:, 0], reduced_data[:, 1],c=result)
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.show()
+###########
+#AffinityPropagation
+af = AffinityPropagation(preference=-50).fit(reduced_data)
+cluster_centers_indices = af.cluster_centers_indices_
+labels = af.labels_
+
+n_clusters_ = 15
+plt.close('all')
+plt.figure(3)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    class_members = labels == k
+    cluster_center = reduced_data[cluster_centers_indices[k]]
+    plt.plot(reduced_data[class_members, 0], reduced_data[class_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=5)
+    for x in reduced_data[class_members]:
+        plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+
+########################
+#DBSCAN 0.3
+db = DBSCAN(eps=0.3, min_samples=10).fit(reduced_data)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 0.2]
+
+    class_member_mask = (labels == k)
+
+    xy = reduced_data[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+    xy = reduced_data[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+#DBSCAN 0.33
+db = DBSCAN(eps=0.33, min_samples=10).fit(reduced_data)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 0.2]
+
+    class_member_mask = (labels == k)
+
+    xy = reduced_data[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+    xy = reduced_data[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+#DBSCAN 0.4
+db = DBSCAN(eps=0.4, min_samples=10).fit(reduced_data)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 0.2]
+
+    class_member_mask = (labels == k)
+
+    xy = reduced_data[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+    xy = reduced_data[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+##################
+'''
+from sklearn.cluster import OPTICS, cluster_optics_dbscan
+import matplotlib.gridspec as gridspec
+
+# OPTICS
+clust = OPTICS(min_samples=50, xi=.05, min_cluster_size=.05)
+# Run the fit
+clust.fit(reduced_data)
+labels_050 = cluster_optics_dbscan(reachability=clust.reachability_,
+                                   core_distances=clust.core_distances_,
+                                   ordering=clust.ordering_, eps=0.5)
+labels_200 = cluster_optics_dbscan(reachability=clust.reachability_,
+                                   core_distances=clust.core_distances_,
+                                   ordering=clust.ordering_, eps=2)
+
+space = np.arange(len(reduced_data))
+reachability = clust.reachability_[clust.ordering_]
+labels = clust.labels_[clust.ordering_]
+colors = ['g.', 'r.', 'b.', 'y.', 'c.']
+for klass, color in zip(range(0, 5), colors):
+    Xk = reduced_data[clust.labels_ == klass]
+    plt.plot(Xk[:, 0], Xk[:, 1], color, alpha=0.3)
+plt.plot([clust.labels_ == -1, 0], reduced_data[clust.labels_ == -1, 1], 'k+', alpha=0.1)
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+'''
---
+++ Data_mining_exp&report_four/README.md
@@ -0,0 +1,19 @@
+# Data mining
+数据班 赵鑫鉴  201700181053
+## Requirements
++ python==3.7.4
++ textblob==0.15.3
++ scikit-learn ==0.21.3（0.20.3以下版本可能会报错）
++ NumPy (>= 1.11.0)
++ SciPy (>= 0.17.0)
++ joblib (>= 0.11)
+
+运行此代码需要安装sklearn，可以参考以下几种下载或者更新方式：
+```
+使用清华镜像下载：
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scikit-learn
+更新：
+pip install --upgrade scikit-learn
+conda update scikit-learn
+pip install -U sklearn
+```
